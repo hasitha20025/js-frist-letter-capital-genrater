4a99cc719900659228621fb1fd2d69483a6d825d
Hasitha Dhananjaya <ict20025@std.uwu.ac.lk>
2023-06-21 10:26:47 +0530

    Update frist leter capital genrater.js

--- frist leter capital genrater.js
+++ frist leter capital genrater.js
@@ -1,4 +1,4 @@
-// js code  frist charcter capital genrater..
+// js code  frist charcter capital genrater
 
 var name = prompt("Ener you name");
 var name_capital = name.slice(0,1);
@@ -6,5 +6,7 @@ var name_capital = name.slice(0,1);
 var frist_capital_char = name_capital.toUpperCase();
 
 var rest_name = name.slice(1,name.length);
+
+var rest_name = rest_name.toLowerCase();
 
 alert(frist_capital_char + rest_name);
